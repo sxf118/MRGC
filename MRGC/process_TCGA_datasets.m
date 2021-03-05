10d1e4091b0a4d2ea37435047853b7cab736e124
sxf118 <80116105+sxf118@users.noreply.github.com>
2021-03-06 00:26:51 +0800

    Add files via upload

---
+++ MRGC/process_TCGA_datasets.m
@@ -0,0 +1,15 @@
+function [U, idx_eg, idx_rc] = process_TCGA_datasets(X, par)
+    U = MRGC(X, par.num_bases, par.alpha, par.beta, par.num_iters);
+    NUMC = 2 : 15;
+    [K1, ~, K12, ~] = Estimate_Number_of_Clusters_given_graph(U, NUMC);
+    fprintf('The number of clusters estimated by eigen gap : %d\n', K1);
+    idx_eg = computeLabels(U, K1);
+    fprintf('The number of clusters estimated by rotation cost : %d\n', K12);
+    idx_rc = computeLabels(U, K12);
+end
+
+
+function idx = computeLabels(T, k)
+    Z = (abs(T)+ abs(T')) / 2;
+    idx = clu_ncut(Z, k);
+end
